6dc6c7321e254a64c503b5d2af0e710c01470808
Edgar Muss <edgar.muss@doctolib.com>
2022-05-29 18:43:17 +0200

    feat: Description v1

---
+++ modules/pokemon/components/Pokemons/Description.tsx
@@ -0,0 +1,22 @@
+import Typography from "@mui/material/Typography";
+
+const Description = () => {
+
+    return (
+        <div className="h-[40vh] bg-gray-800 flex flex-col justify-evenly items-center py-8">
+            <Typography variant="h1" component="h1" className="text-gray-300">
+                Pokémon
+            </Typography>
+            <div className="text-gray-300">
+                <Typography variant="subtitle1" component="h2">
+                    Explore the fantastic world of Pokémons.
+                </Typography>
+                <Typography variant="body1" component="div">
+                    Search the list for Pokémons that have always interested you or filter directly by them.
+                </Typography>
+            </div>
+        </div>
+    )
+}
+
+export default Description
