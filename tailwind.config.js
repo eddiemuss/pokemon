4678c11074e7c2ad343ad1a24d46cd75af646220
Edgar Muss <edgar.muss@doctolib.com>
2022-05-29 17:57:56 +0200

    remove files and move global.css into common

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./common/components/**/*.{js,ts,jsx,tsx}",
+        "./modules/*/components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
